28fdab25ebfbc3c5fdc7878ed024bbb2eb820b37
Caaronbll <aaronbll46@gmail.com>
2023-12-14 13:54:20 -0600

    took out sky and dimmed light

--- forest/main1.js
+++ forest/main1.js
@@ -133,6 +133,7 @@ class FirstPersonCamera {
   updateCamera_(_) {
     this.camera_.quaternion.copy(this.rotation_);
     this.camera_.position.copy(this.translation_);
+    this.camera_.position.y += Math.sin(this.headBobTimer_ * 10) * 1.5;
 
     const forward = new THREE.Vector3(0, 0, -1);
     forward.applyQuaternion(this.rotation_);
@@ -165,17 +166,21 @@ class FirstPersonCamera {
 
     const forward = new THREE.Vector3(0, 0, -1);
     forward.applyQuaternion(qx);
-    forward.multiplyScalar(forwardVelocity * timeElapsedS * 5);
+    forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
 
     const left = new THREE.Vector3(-1, 0, 0);
     left.applyQuaternion(qx);
-    left.multiplyScalar(strafeVelocity * timeElapsedS * 5);
+    left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
 
     this.translation_.add(forward);
     this.translation_.add(left);
+
+    if (forwardVelocity != 0 || strafeVelocity != 0) {
+      this.headBobActive_ = true;
+    }
   }
 
-  updateRotation_() {
+  updateRotation_(timeElapsedS) {
     const xh = this.input_.current_.mouseXDelta / window.innerWidth;
     const yh = this.input_.current_.mouseYDelta / window.innerHeight;
 
@@ -205,6 +210,7 @@ class FirstPersonCameraDemo {
     this.initializeRenderer_();
     this.initializeLights_();
     this.initializeScene_();
+    this.initializePostFX_();
     this.initializeDemo_();
 
     this.previousRAF_ = null;
@@ -213,6 +219,11 @@ class FirstPersonCameraDemo {
   }
 
   initializeDemo_() {
+    // this.controls_ = new FirstPersonControls(
+    //     this.camera_, this.threejs_.domElement);
+    // this.controls_.lookSpeed = 0.8;
+    // this.controls_.movementSpeed = 5;
+
     this.fpsCamera_ = new FirstPersonCamera(this.camera_, this.objects_);
   }
 
@@ -248,8 +259,19 @@ class FirstPersonCameraDemo {
   }
 
   initializeScene_() {
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      './resources/skybox/posx.jpg',
+      './resources/skybox/negx.jpg',
+      './resources/skybox/posy.jpg',
+      './resources/skybox/negy.jpg',
+      './resources/skybox/posz.jpg',
+      './resources/skybox/negz.jpg',
+  ]);
 
-    // LOADS THE FLOOR / GRASS //
+    texture.encoding = THREE.sRGBEncoding;
+    this.scene_.background = texture;
+
     const mapLoader = new THREE.TextureLoader();
     const maxAnisotropy = this.threejs_.capabilities.getMaxAnisotropy();
     const checkerboard = mapLoader.load('resources/pexels-pixmike-413195.jpg');
@@ -260,17 +282,59 @@ class FirstPersonCameraDemo {
     checkerboard.encoding = THREE.sRGBEncoding;
 
     const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(100, 100, 10, 10),
-    new THREE.MeshStandardMaterial({map: checkerboard}));
+        new THREE.PlaneGeometry(100, 100, 10, 10),
+        new THREE.MeshStandardMaterial({map: checkerboard}));
     plane.castShadow = false;
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
     this.scene_.add(plane);
 
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 4, 4),
+      this.loadMaterial_('vintage-tile1_', 0.2));
+    box.position.set(10, 2, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    this.scene_.add(box);
+
+    const concreteMaterial = this.loadMaterial_('concrete3-', 4);
+
+    const wall1 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 120, 4),
+      concreteMaterial);
+    wall1.position.set(0, -40, -50);
+    wall1.castShadow = true;
+    wall1.receiveShadow = true;
+    this.scene_.add(wall1);
+
+    const wall2 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 120, 4),
+      concreteMaterial);
+    wall2.position.set(0, -40, 50);
+    wall2.castShadow = true;
+    wall2.receiveShadow = true;
+    this.scene_.add(wall2);
+
+    const wall3 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 120, 100),
+      concreteMaterial);
+    wall3.position.set(50, -40, 0);
+    wall3.castShadow = true;
+    wall3.receiveShadow = true;
+    this.scene_.add(wall3);
+
+    const wall4 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 120, 100),
+      concreteMaterial);
+    wall4.position.set(-50, -40, 0);
+    wall4.castShadow = true;
+    wall4.receiveShadow = true;
+    this.scene_.add(wall4);
+
     // Create Box3 for each mesh in the scene so that we can
     // do some easy intersection tests.
     const meshes = [
-      plane];
+      plane, box, wall1, wall2, wall3, wall4];
 
     this.objects_ = [];
 
@@ -280,27 +344,26 @@ class FirstPersonCameraDemo {
       this.objects_.push(b);
     }
 
-    // CROSSHAIR //
+    // Crosshair
     const crosshair = mapLoader.load('resources/crosshair.png');
     crosshair.anisotropy = maxAnisotropy;
 
     this.sprite_ = new THREE.Sprite(
-    new THREE.SpriteMaterial({map: crosshair, color: 0xffffff, fog: false, depthTest: false, depthWrite: false}));
+      new THREE.SpriteMaterial({map: crosshair, color: 0xffffff, fog: false, depthTest: false, depthWrite: false}));
     this.sprite_.scale.set(0.15, 0.15 * this.camera_.aspect, 1)
     this.sprite_.position.set(0, 0, -10);
 
     this.uiScene_.add(this.sprite_);
   }
 
-  // LIGHTING //
   initializeLights_() {
-    const distance = 10.0;
+    const distance = 50.0;
     const angle = Math.PI / 4.0;
     const penumbra = 0.5;
-    const decay = 2.0;
+    const decay = 1.0;
 
     let light = new THREE.SpotLight(
-      0x0B5394, 100.0, distance, angle, penumbra, decay);
+        0xFFFFFF, 100.0, distance, angle, penumbra, decay);
     light.castShadow = true;
     light.shadow.bias = -0.00001;
     light.shadow.mapSize.width = 4096;
@@ -312,13 +375,55 @@ class FirstPersonCameraDemo {
     light.lookAt(0, 0, 0);
     this.scene_.add(light);
 
-    const upColour = 0x0B5394;
+    const upColour = 0xFFFF80;
     const downColour = 0x808080;
     light = new THREE.HemisphereLight(upColour, downColour, 0.5);
     light.color.setHSL( 0.6, 1, 0.6 );
     light.groundColor.setHSL( 0.095, 1, 0.75 );
-    light.position.set(0, 40, 0);
+    light.position.set(0, 4, 0);
     this.scene_.add(light);
+  }
+
+  loadMaterial_(name, tiling) {
+    const mapLoader = new THREE.TextureLoader();
+    const maxAnisotropy = this.threejs_.capabilities.getMaxAnisotropy();
+
+    const metalMap = mapLoader.load('resources/freepbr/' + name + 'metallic.png');
+    metalMap.anisotropy = maxAnisotropy;
+    metalMap.wrapS = THREE.RepeatWrapping;
+    metalMap.wrapT = THREE.RepeatWrapping;
+    metalMap.repeat.set(tiling, tiling);
+
+    const albedo = mapLoader.load('resources/freepbr/' + name + 'albedo.png');
+    albedo.anisotropy = maxAnisotropy;
+    albedo.wrapS = THREE.RepeatWrapping;
+    albedo.wrapT = THREE.RepeatWrapping;
+    albedo.repeat.set(tiling, tiling);
+    albedo.encoding = THREE.sRGBEncoding;
+
+    const normalMap = mapLoader.load('resources/freepbr/' + name + 'normal.png');
+    normalMap.anisotropy = maxAnisotropy;
+    normalMap.wrapS = THREE.RepeatWrapping;
+    normalMap.wrapT = THREE.RepeatWrapping;
+    normalMap.repeat.set(tiling, tiling);
+
+    const roughnessMap = mapLoader.load('resources/freepbr/' + name + 'roughness.png');
+    roughnessMap.anisotropy = maxAnisotropy;
+    roughnessMap.wrapS = THREE.RepeatWrapping;
+    roughnessMap.wrapT = THREE.RepeatWrapping;
+    roughnessMap.repeat.set(tiling, tiling);
+
+    const material = new THREE.MeshStandardMaterial({
+      metalnessMap: metalMap,
+      map: albedo,
+      normalMap: normalMap,
+      roughnessMap: roughnessMap,
+    });
+
+    return material;
+  }
+
+  initializePostFX_() {
   }
 
   onWindowResize_() {
@@ -350,6 +455,8 @@ class FirstPersonCameraDemo {
 
   step_(timeElapsed) {
     const timeElapsedS = timeElapsed * 0.001;
+
+    // this.controls_.update(timeElapsedS);
     this.fpsCamera_.update(timeElapsedS);
   }
 }
